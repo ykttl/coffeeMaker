b969b17f4b15f49a4ddeb19503f43ab188d984f6
Yuko <yuko.mazurek@gmail.com>
2018-06-21 20:19:04 -0700

    made divs in HTML instead of creatingElements in JS. fixed bug of displaying foam again.

--- app.js
+++ app.js
@@ -1,36 +1,29 @@
+
 var data = {
 
 	coffeeName: '',		
 
-	  espresso: {
+	espresso: {
+		sliderValue: 0,
+		slider0: true,
+	    slider1: false,
+	    height: 0,
+	},
 
-		 sliderValue: 0,
-		 slider0: true,
-	     slider1: false,
-	     height: 0,
-
-	  },
-
-      milk: {
-
+    milk: {
 		 sliderValue: 0,
 		 slider0: true,
 	     slider1: false,
 		 slider2: false,
 	     height: 0,
+	}, 
 
-	  }, 
-
-
-	  foam: {
-
+	foam: {
 		 sliderValue: 0,
 		 slider0: true,
 	     slider1: false,
 	     height: 0,
-
-      },
-
+    },
 };
 
 
@@ -45,8 +38,7 @@ var event = {
 
 	espressoSlider: function() { 
 		document.getElementById('espresso-slider').addEventListener('change', function() {
-
-			var slider = document.getElementById('espresso-slider').value; // this returns number as string
+		var slider = document.getElementById('espresso-slider').value; // this returns number as string
 
 			if(slider == 1 && data.espresso.slider0 === true) { 
 				console.log( '0 -> 1');
@@ -59,7 +51,7 @@ var event = {
 				data.espresso.sliderValue = 0;
 				data.espresso.slider1 = false;
 				data.espresso.slider0 = true;
-				render.removeEspresso();
+				render.espresso();
 			}
 	        if(slider == 0 && data.espresso.slider1 === false ){ console.log( '0');
 				data.espresso.sliderValue = 0;
@@ -70,7 +62,8 @@ var event = {
 			render.coffeeName();
 
 
-			var foam = document.getElementById('foam');
+		var foam = document.getElementById('foam');
+
              if (data.foam.height != 0 && data.milk.sliderValue === 0) {
              	foam.style.bottom = data.espresso.height; 
              }
@@ -83,7 +76,7 @@ var event = {
 
 	milkSlider: function() {
 		document.getElementById('milk-slider').addEventListener('change', function() {
-			var slider = document.getElementById('milk-slider').value; // this returns number as string
+		var slider = document.getElementById('milk-slider').value; // this returns number as string
 
 			if(slider == 2) { console.log( '1 -> 2');
 				data.milk.sliderValue = 2;
@@ -98,8 +91,6 @@ var event = {
 				data.milk.slider2 = false;
 				data.milk.slider1 = true;
 				data.milk.slider0 = false;
-				render.removeMilk();
-				render.removeMilk();
 				render.milk();
 			}
 			if(slider == 1 && data.milk.slider2 === false && data.milk.slider0 === true) { 
@@ -109,25 +100,20 @@ var event = {
 				data.milk.slider1 = true;
 				data.milk.slider0 = false;
 				render.milk();
-			}
-			
+			}			
 			if(slider == 0 && data.milk.slider1 === true ){ console.log( '1 -> 0');
 				data.milk.sliderValue = 0;
 				data.milk.slider2 = false;
 				data.milk.slider1 = false;
 				data.milk.slider0 = true;
-				render.removeMilk();
+				render.milk();				
 			}
-			
-
 			if(slider == 0 && data.milk.slider2 === true ){ console.log( '2 -> 0');
-				data.milk.sliderValue = 0;
+				data.milk.sliderValue = 0; 
 				data.milk.slider2 = false;
 				data.milk.slider1 = false;
 				data.milk.slider0 = true;
-				render.removeMilk();
-				render.removeMilk();
-
+				render.milk();				
 			}
 	        if(slider == 0 && data.milk.slider1 === false ){ console.log( '0');
 				data.milk.sliderValue = 0;
@@ -138,22 +124,23 @@ var event = {
 
 			render.coffeeName();
 			
-			var foam = document.getElementById('foam');
-            if(data.foam.height != 0) {
+		var foam = document.getElementById('foam');
+
+            if(data.foam.height != 0 && data.milk.sliderValue != 0) {
             	foam.style.bottom = data.milk.height; 
              }  
-             if (data.foam.height != 0 && data.milk.sliderValue === 0) {
+             if (data.foam.height != 0 && data.milk.sliderValue === 0 && data.espresso.sliderValue === 0) {
              	foam.style.bottom = 0; 
              }
-
+             if (data.foam.height != 0 && data.milk.sliderValue === 0 && data.espresso.sliderValue != 0) {
+             	foam.style.bottom = data.espresso.height; 
+             }
 	    });
 	},
 
 	foamSlider: function() {
-		document.getElementById('foam-slider').addEventListener('change', function() {
-		
+		document.getElementById('foam-slider').addEventListener('change', function() {		
 			var slider = document.getElementById('foam-slider').value; // this returns number as string
-
 
 			if(slider == 1  && data.foam.slider0 === true) { 
 				console.log( '0 -> 1');
@@ -166,16 +153,15 @@ var event = {
 				data.foam.sliderValue = 0;
 				data.foam.slider1 = false;
 				data.foam.slider0 = true;
-				render.removeFoam();
+				render.foam();
 			}
 	        if(slider == 0 && data.foam.slider1 === false ){ console.log( '0');
 				data.foam.sliderValue = 0;
 				data.foam.slider1 = false;
-				data.foam.slider0 = true;			
+				data.foam.slider0 = true;	
 			}
 
-			render.coffeeName();
-            
+			render.coffeeName();            
 		})
 	},
 };
@@ -183,11 +169,9 @@ var event = {
 
 var render = {
 
-
 	espresso: function() {	  
-		var espresso = document.createElement('div');
-			espresso.setAttribute('id','espresso');
-			document.getElementById('magcup').appendChild(espresso);
+		
+		var espresso = document.getElementById('espresso');
 
 		if (data.espresso.sliderValue === 1) { 	
 		  	data.espresso.height = '30px';
@@ -200,15 +184,9 @@ var render = {
 		 espresso.style.height = data.espresso.height;		 
 	},
 
-	removeEspresso: function() {
-        var espresso = document.getElementById('espresso');
-	        espresso.parentNode.removeChild(espresso);
-	},
-
 	milk: function() {		
-		var milk = document.createElement('div');
-			milk.setAttribute('id','milk');
-			document.getElementById('magcup').appendChild(milk);
+
+		var milk = document.getElementById('milk');
 
 		if (data.milk.sliderValue === 1) { 		
 		  	data.milk.height = '80px';
@@ -222,25 +200,20 @@ var render = {
 			data.milk.height= '0px';			
 		}
 
-		 milk.style.height = data.milk.height;	
+		 milk.style.height = data.milk.height;	console.log(data.milk.height);
 	},
 
-	removeMilk: function() {
-        var milk = document.getElementById('milk');
-	        milk.parentNode.removeChild(milk);
-	},
-
-	foam: function() {				 
-		var foam = document.createElement('div');
-			foam.setAttribute('id','foam');
-			document.getElementById('magcup').appendChild(foam);
+	foam: function() {						
+		var foam = document.getElementById('foam');
 
 		if (data.foam.sliderValue === 1) { 	
 		  	data.foam.height = '40px';
+		  	foam.style.border = 'dashed 2px gray';
 		} 
 
 		if (data.foam.sliderValue === 0) {
-			data.foam.height= '0px';			
+			data.foam.height= '0px';
+			foam.style.border = 'none';
 		}
 
 		if(data.milk.sliderValue != 0){
@@ -255,11 +228,6 @@ var render = {
 
 		foam.style.height = data.foam.height;
 		
-	},
-
-	removeFoam: function() {
-        var foam = document.getElementById('foam');
-	        foam.parentNode.removeChild(foam);
 	},
 
 	coffeeName: function () {
@@ -294,7 +262,6 @@ var render = {
 
 		var name = document.getElementById('coffee-name');
 		name.textContent = data.coffeeName;
-
 
 		this.menu(); 
 
